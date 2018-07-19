3b7e547072f5a8e1c768a9afbb9d9376eb14e482
DennySoul <mr.denis.chernysh@gmail.com>
2018-07-19 11:24:42 +0300

    wire up front-end to aws backend

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
 
+import Amplify from 'aws-amplify';
+import config from 'src/aws-exports';
+
+// link front-end and aws backend together
+Amplify.configure(config);
+
 ReactDOM.render(<Application />, document.getElementById('root'));
