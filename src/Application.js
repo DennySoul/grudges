1c01b3d1a8fc15189ab46bb2bf8159c519ffed83
Denis Chernysh <mr.denis.chernysh@gmail.com>
2018-07-19 16:59:14 +0300

    Dc aws workshop (#1)
    * wire up front-end to aws backend
    
    * add authenticator
    
    * added CRUD operations

--- src/Application.js
+++ src/Application.js
@@ -3,52 +3,74 @@ import NewGrudge from './NewGrudge';
 import Grudges from './Grudges';
 import './Application.css';
 
+import { withAuthenticator } from 'aws-amplify-react';
+import { API } from 'aws-amplify';
+
 class Application extends Component {
-  state = {
-    grudges: [],
-  };
+    state = {
+        grudges: [],
+    };
 
-  addGrudge = grudge => {
-    this.setState({ grudges: [grudge, ...this.state.grudges] });
-  };
+    componentDidMount() {
+        API.get('aws-workshopCRUD', '/aws-workshop')
+            .then(res => {
+                this.setState((prevState, state) => ({ ...prevState, grudges: res }));
+            })
+    }
 
-  removeGrudge = grudge => {
-    this.setState({
-      grudges: this.state.grudges.filter(other => other.id !== grudge.id),
-    });
-  };
+    addGrudge = grudge => {
+        API.post('aws-workshopCRUD', '/aws-workshop', { body: grudge })
+            .then(() => this.setState({ grudges: [grudge, ...this.state.grudges] }));
+    };
 
-  toggle = grudge => {
-    const othergrudges = this.state.grudges.filter(
-      other => other.id !== grudge.id,
-    );
-    const updatedGrudge = { ...grudge, avenged: !grudge.avenged };
-    this.setState({ grudges: [updatedGrudge, ...othergrudges] });
-  };
+    removeGrudge = ({ id }) => {
+        API.get('aws-workshopCRUD', `/aws-workshop/object/${id}`)
+            .then(() =>
+                this.setState({
+                    grudges: this.state.grudges.filter(other => other.id !== id),
+                }
+            ));
+    };
 
-  render() {
-    const { grudges } = this.state;
-    const unavengedgrudges = grudges.filter(grudge => !grudge.avenged);
-    const avengedgrudges = grudges.filter(grudge => grudge.avenged);
+    toggle = grudge => {
+        const updatedGrudge = {
+            ...grudge,
+            avenged: !grudge.avenged
+        };
 
-    return (
-      <div className="Application">
-        <NewGrudge onSubmit={this.addGrudge} />
-        <Grudges
-          title="Unavenged Grudges"
-          grudges={unavengedgrudges}
-          onCheckOff={this.toggle}
-          onRemove={this.removeGrudge}
-        />
-        <Grudges
-          title="Avenged Grudges"
-          grudges={avengedgrudges}
-          onCheckOff={this.toggle}
-          onRemove={this.removeGrudge}
-        />
-      </div>
-    );
-  }
+        API.put('aws-workshopCRUD', '/aws-workshop', { body: updatedGrudge })
+            .then(() => {
+                const othergrudges = this.state.grudges.filter(
+                    other => other.id !== grudge.id,
+                );
+
+                this.setState({ grudges: [updatedGrudge, ...othergrudges] });
+            });
+    };
+
+    render() {
+        const { grudges } = this.state;
+        const unavengedgrudges = grudges.filter(grudge => !grudge.avenged);
+        const avengedgrudges = grudges.filter(grudge => grudge.avenged);
+
+        return (
+            <div className="Application">
+                <NewGrudge onSubmit={this.addGrudge}/>
+                <Grudges
+                    title="Unavenged Grudges"
+                    grudges={unavengedgrudges}
+                    onCheckOff={this.toggle}
+                    onRemove={this.removeGrudge}
+                />
+                <Grudges
+                    title="Avenged Grudges"
+                    grudges={avengedgrudges}
+                    onCheckOff={this.toggle}
+                    onRemove={this.removeGrudge}
+                />
+            </div>
+        );
+    }
 }
 
-export default Application;
+export default withAuthenticator(Application);
